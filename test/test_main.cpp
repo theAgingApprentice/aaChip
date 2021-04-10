b8e345d53e97fa8b22236cb9f296b26a3334dfd8
va3wam <va3wam@gmail.com>
2021-04-09 23:11:28 -0400

    Updated to contain dummy tests.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,34 +1,69 @@
- #include <Arduino.h>
-#include <aaChip.h>
+#include <Arduino.h>
+#include <unity.h>
 
-aaChip core1; // Arduino runs on the AppCPU (core1).
+String STR_TO_TEST;
 
-/**
- * @brief Initialize the serial output with the specified baud rate measured in bits 
- * per second.
- * =================================================================================*/
-void setupSerial()
+void setUp(void) 
 {
-   unsigned long serialBaudRate = 115200;
-   Serial.begin(serialBaudRate); // Open a serial connection at specified baud rate. 
-   while (!Serial); // Wait for Serial port to be ready.
-} //setupSerial()
+    // set stuff up here
+    STR_TO_TEST = "Hello, world!";
+}
 
-/** 
- * @brief Arduino mandatory function #1. Runs once at boot. 
- * =================================================================================*/
-void setup() 
+void tearDown(void) 
 {
-   setupSerial(); // Set serial baud rate. 
-   Serial.println("<setup> Start of setup");
-   core1.cfgToConsole();
-   Serial.println("<setup> End of setup");
-} //setup()
+    // clean stuff up here
+    STR_TO_TEST = "";
+}
 
-/**
- * @brief Arduino mandatory function #2. Runs continually.
- * =================================================================================*/
-void loop() 
+void test_string_concat(void) 
 {
-  
-} //loop()
+    String hello = "Hello, ";
+    String world = "world!";
+    TEST_ASSERT_EQUAL_STRING(STR_TO_TEST.c_str(), (hello + world).c_str());
+}
+
+void test_string_substring(void) 
+{
+    TEST_ASSERT_EQUAL_STRING("Hello", STR_TO_TEST.substring(0, 5).c_str());
+}
+
+void test_string_index_of(void) 
+{
+    TEST_ASSERT_EQUAL(7, STR_TO_TEST.indexOf('w'));
+}
+
+void test_string_equal_ignore_case(void) 
+{
+    TEST_ASSERT_TRUE(STR_TO_TEST.equalsIgnoreCase("HELLO, WORLD!"));
+}
+
+void test_string_to_upper_case(void) 
+{
+    STR_TO_TEST.toUpperCase();
+    TEST_ASSERT_EQUAL_STRING("HELLO, WORLD!", STR_TO_TEST.c_str());
+}
+
+void test_string_replace(void) 
+{
+    STR_TO_TEST.replace('!', '?');
+    TEST_ASSERT_EQUAL_STRING("Hello, world?", STR_TO_TEST.c_str());
+}
+
+void setup()
+{
+    delay(2000); // service delay
+    UNITY_BEGIN();
+
+    RUN_TEST(test_string_concat);
+    RUN_TEST(test_string_substring);
+    RUN_TEST(test_string_index_of);
+    RUN_TEST(test_string_equal_ignore_case);
+    RUN_TEST(test_string_to_upper_case);
+    RUN_TEST(test_string_replace);
+
+    UNITY_END(); // stop unit testing
+}
+
+void loop()
+{
+}
